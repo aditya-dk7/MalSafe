313709e39977022ae1680a4dbfa596b671f3334b
Aditya Ramesh <rameshaditya2001@gmail.com>
2021-12-04 13:18:48 -0500

    Editied main.py to get extract PEInformation from exe

--- PEModule/main.py
+++ PEModule/main.py
@@ -1,4 +1,69 @@
 import pefile
+import array
+import math
+
+def getEntropy(data):
+    if len(data) == 0:
+        return 0.0
+    occurences = array.array('L', [0] * 256)
+    for x in data:
+        occurences[x if isinstance(x, int) else ord(x)] += 1
+
+    entropy = 0
+    for x in occurences:
+        if x:
+            p_x = float(x) / len(data)
+            entropy -= p_x * math.log(p_x, 2)
+
+    return entropy
+
+
+def getResources(pe):
+    """Extract resources :
+    [entropy, size]"""
+    resources = []
+    if hasattr(pe, 'DIRECTORY_ENTRY_RESOURCE'):
+        try:
+            for resource_type in pe.DIRECTORY_ENTRY_RESOURCE.entries:
+                if hasattr(resource_type, 'directory'):
+                    for resource_id in resource_type.directory.entries:
+                        if hasattr(resource_id, 'directory'):
+                            for resource_lang in resource_id.directory.entries:
+                                data = pe.get_data(resource_lang.data.struct.OffsetToData,
+                                                   resource_lang.data.struct.Size)
+                                size = resource_lang.data.struct.Size
+                                entropy = getEntropy(data)
+
+                                resources.append([entropy, size])
+        except Exception as e:
+            return resources
+    return resources
+
+
+def getVersionInfo(pe):
+    #Return version infos
+    result = {}
+    for fileinfo in pe.FileInfo:
+        if fileinfo.Key == 'StringFileInfo':
+            for st in fileinfo.StringTable:
+                for entry in st.entries.items():
+                    result[entry[0]] = entry[1]
+        if fileinfo.Key == 'VarFileInfo':
+            for var in fileinfo.Var:
+                result[var.entry.items()[0][0]] = var.entry.items()[0][1]
+    if hasattr(pe, 'VS_FIXEDFILEINFO'):
+        result['flags'] = pe.VS_FIXEDFILEINFO.FileFlags
+        result['os'] = pe.VS_FIXEDFILEINFO.FileOS
+        result['type'] = pe.VS_FIXEDFILEINFO.FileType
+        result['file_version'] = pe.VS_FIXEDFILEINFO.FileVersionLS
+        result['product_version'] = pe.VS_FIXEDFILEINFO.ProductVersionLS
+        result['signature'] = pe.VS_FIXEDFILEINFO.Signature
+        result['struct_version'] = pe.VS_FIXEDFILEINFO.StrucVersion
+    return result
+
+
+
+
 def extractAllPeinfo(file_path):
     pe = pefile.PE(file_path)
     result = {}
@@ -71,14 +136,41 @@ def extractAllPeinfo(file_path):
         result['ExportNb'] = len(pe.DIRECTORY_ENTRY_EXPORT.symbols)
     except AttributeError:
         result['ExportNb'] = 0
-    
+
+    resources = getResources(pe)
+    result['ResourcesNb'] = len(resources)
+    if len(resources) > 0:
+        entropy = list(map(lambda x: x[0], resources))
+        result['ResourcesMeanEntropy'] = sum(entropy) / float(len(entropy))
+        result['ResourcesMinEntropy'] = min(entropy)
+        result['ResourcesMaxEntropy'] = max(entropy)
+        sizes = list(map(lambda x: x[1], resources))
+        result['ResourcesMeanSize'] = sum(sizes) / float(len(sizes))
+        result['ResourcesMinSize'] = min(sizes)
+        result['ResourcesMaxSize'] = max(sizes)
+    else:
+        result['ResourcesNb'] = 0
+        result['ResourcesMeanEntropy'] = 0
+        result['ResourcesMinEntropy'] = 0
+        result['ResourcesMaxEntropy'] = 0
+        result['ResourcesMeanSize'] = 0
+        result['ResourcesMinSize'] = 0
+        result['ResourcesMaxSize'] = 0
+    try:
+        version_infos = getVersionInfo(pe)
+        result['VersionInformationSize'] = len(version_infos.keys())
+    except AttributeError:
+        result['VersionInformationSize'] = 0
     try:
         result['LoadConfigurationSize'] = pe.DIRECTORY_ENTRY_LOAD_CONFIG.struct.Size
     except AttributeError:
         result['LoadConfigurationSize'] = 0
     
+    return result
+    
     
 
 pe = pefile.PE('exe/NonMalicious/JRuler.exe')
+print(extractAllPeinfo('exe/NonMalicious/JRuler.exe'))
 
 
