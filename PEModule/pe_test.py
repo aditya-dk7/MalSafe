e22bd23e000803c8f184fa9d3db75741f7dd5d9d
Aditya Ramesh <rameshaditya2001@gmail.com>
2021-12-10 14:56:05 -0500

    Code updates

--- PEModule/pe_test.py
+++ PEModule/pe_test.py
@@ -1,11 +1,36 @@
 import joblib
 import pickle
 import os
+import argparse
+import pefile
 from util import mainPE
-clf = joblib.load('/home/dk7/projectsCap/MalSafe/PEModule/savedmodel/model_jlib.pkl')
-features = pickle.loads(open('/home/dk7/projectsCap/MalSafe/PEModule/savedmodel/model_jlib_features.pkl', mode='rb').read())
-data = mainPE.extractAllPeinfo('/home/dk7/projectsCap/MalSafe/PEModule/exe/NonMalicious/Firefox.exe')
-pe_features = map(lambda x: data[x], features)
-x = list(pe_features)
-res = clf.predict([x])[0]
-print('The file is %s' % (['malicious', 'legitimate'][res]))
+
+
+def predictMalicious(filePath):
+    picklePth = os.path.join(os.getcwd(), 'savedmodel', 'model_jlib.pkl')
+    pickleFeaturePth = os.path.join(os.getcwd(), 'savedmodel', 'model_jlib_features.pkl')
+    print(picklePth)
+    clf = joblib.load(picklePth)
+    features = pickle.loads(open(pickleFeaturePth, mode='rb').read())
+    data = mainPE.extractAllPeinfo(filePath)
+    pe_features = map(lambda x: data[x], features)
+    x = list(pe_features)
+    res = clf.predict([x])[0]
+    print('The file is %s' % (['malicious', 'legitimate'][res]))
+
+
+def main():
+     parser = argparse.ArgumentParser(description='Predicts whether file is malicious or not using PE Information')
+     parser.add_argument('-f', '--file', help='The file to predict malicious or not', required=True)
+     args = parser.parse_args()
+     try:
+         pefile.PE(args.file)
+         predictMalicious(args.file)
+     except pefile.PEFormatError:
+         print("[-]The file does not contain PE Information.")
+     
+if __name__ == '__main__':
+    main()
+
+
+
