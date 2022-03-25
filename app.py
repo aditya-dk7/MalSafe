c438f82a8f51c5871d6a397a8c77f4830f19b022
Aditya Ramesh <rameshaditya2001@gmail.com>
2022-03-25 17:28:59 -0400

    new Changes

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template, request
 import os
-from flask import jsonify
-from flask_restful import Api, Resource
-import base64
 import pefile
 import URLModule.url_ssl_verf as url_ssl_verf
 import PEModule.pe_test as pe_test
@@ -26,28 +23,36 @@ def run():
 @app.route("/submit", methods = ['GET', 'POST'])
 def get_output():
 	if request.method == 'POST':
-		img = request.files['my_image']
-
-		img_path = "static/" + img.filename	
-		img.save(img_path)
-		filename = os.path.join(os.path.dirname("static/"), img.filename)
-		string = 'exiftool/exiftool -j ' + filename
-		metaInfo = os.popen(string).read()
-		print(metaInfo)
-		metaInfo = json.loads(metaInfo)
-		fileResult = {}
-		fileResult['metaInfo'] = metaInfo
-		fileResult['md5'] = "0bfb331611cbcf420b38f73e1936f836"
-		if "PEType" in metaInfo[0]:
-			try:
-				pefile.PE(filename)
-				fileResult['peInfoMalicious'] = pe_test.predictMalicious(filename)
-			except:
-				fileResult['peInfoMalicious'] = {}
-		elif metaInfo[0]["FileType"] == "JPEG":
-			fileResult['JPGMalicious']= JPEG_test.check_JPG_malicious(filename)
-	return render_template("index.html", prediction = fileResult, img_path = img_path)
-
+		img = request.files['image_input']
+		url = request.form['url_input']
+		if img.filename != "":
+			img_path = "static/" + img.filename	
+			img.save(img_path)
+			filename = os.path.join(os.path.dirname("static/"), img.filename)
+			string = 'exiftool/exiftool -j ' + filename
+			metaInfo = os.popen(string).read()
+			metaInfo = json.loads(metaInfo)
+			fileResult = {}
+			fileResult['metaInfo'] = metaInfo
+			fileResult['md5'] = "0bfb331611cbcf420b38f73e1936f836"
+			if "PEType" in metaInfo[0]:
+				try:
+					pefile.PE(filename)
+					fileResult['peInfoMalicious'] = pe_test.predictMalicious(filename)
+					fileResult['type'] = 1
+				except:
+					fileResult['peInfoMalicious'] = {}
+					fileResult['type'] = 1
+			elif metaInfo[0]["FileType"] == "JPEG":
+				fileResult['JPGMalicious']= JPEG_test.check_JPG_malicious(filename)
+				fileResult['type'] = 2
+			return render_template("result.html", prediction = fileResult)
+		else:
+			urlResult = {}
+			urlResult['cert_info'] = url_ssl_verf.url_cert_info(url)
+			urlResult['phishing_info']=phishing.check_URL_malicious(url)
+			urlResult['type'] = 3
+			return render_template("result.html", prediction = urlResult)
 
 if __name__ =='__main__':
 	#app.debug = True
